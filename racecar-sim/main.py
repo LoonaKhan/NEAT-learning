21a69e5abd7fa6b71a8afeee09ff48acd1d39a10
MonEmperor <46490084+LoonaKhan@users.noreply.github.com>
2024-01-26 01:58:07 -0500

    can draw and debug rects

--- racecar-sim/main.py
+++ racecar-sim/main.py
@@ -1,13 +1,10 @@
 import pygame
 import math
 
-inner_ring, outer_ring = pygame.sprite.Sprite, pygame.sprite.Sprite
-inner_ring.rect, outer_ring.rect = pygame.Rect, pygame.Rect
 
 
 def dotProduct(direction: int, line: tuple):
     # takes direction and a line coords (edge to centre), finds the angle of the line
-    # todo: store the lines for the rectangle sides. assign them to each side?
     return
 
 
@@ -15,11 +12,11 @@ class Car(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.x,self.y = 150, 200
+        self.x, self.y = 150, 200
         self.v = 0
         self.a = 0
-        self.vmax = 10 # magnitude
-        self.direction = 0 # used for sprite drawing and direction
+        self.vmax = 10  # magnitude
+        self.direction = 0  # used for sprite drawing and direction
 
         self.image = pygame.image.load("direction.png")
         self.rect = self.image.get_rect()
@@ -36,7 +33,7 @@ class Car(pygame.sprite.Sprite):
         elif pygame.key.get_pressed()[pygame.K_s]:
             self.a = 0.2
         else:
-            self.a =0
+            self.a = 0
         # if pygame.key.get_pressed()[pygame.K_a]:
         #     self.ax = -0.2
         # elif pygame.key.get_pressed()[pygame.K_d]:
@@ -47,16 +44,16 @@ class Car(pygame.sprite.Sprite):
         # adds acceleration to velocity.
         # range: [-vmax, vmax]
         self.v = max(min(self.v + self.a, self.vmax), -self.vmax)
-        #self.vy = max(min(self.vy + self.ay, self.vmax), -self.vmax)
+        # self.vy = max(min(self.vy + self.ay, self.vmax), -self.vmax)
         if self.a == 0:
-            self.v += (0-self.v)/50
+            self.v += (0 - self.v) / 50
         # if self.ay == 0:
         #     self.vy += (0-self.vy)/50
 
         # sets direction.
         # uses radians
         if pygame.key.get_pressed()[pygame.K_d]:
-            self.direction += math.pi/50
+            self.direction += math.pi / 50
 
             # self.image = pygame.transform.scale(
             #     pygame.transform.rotate(self.image, -(math.pi/50 * (180/math.pi))),
@@ -71,11 +68,7 @@ class Car(pygame.sprite.Sprite):
             #     [10, 50]
             # )  # the sprite doesnt rotate properly
 
-
-
         # handles collisions
-
-
 
         # print(int(inner_ring.right))
         # if (self.rect.left < inner_ring.rect.right or self.rect.left < outer_ring.rect.right) and self.vx <0:
@@ -88,63 +81,114 @@ class Car(pygame.sprite.Sprite):
         #     self.vy = 0
         self.rect.x += self.v * math.cos(self.direction)
         self.rect.y += self.v * math.sin(self.direction)
-    
+
     def isColliding(self, inner_ring, outer_ring):
         # cannot go out of bounds or through barriers
         # return true if colliding with barriers
-        if (self.rect.x <0 or
+        # todo: check if colliding with a given barrier, if so, determine the direction and dont let the car move unless the dot prodct between the 2 is <=0
+        if (self.rect.x < 0 or
                 self.rect.x > 1920 or
-            pygame.sprite.collide_rect(self, inner_ring) or
-            pygame.sprite.collide_rect(self, outer_ring)
+                pygame.sprite.collide_rect(self, inner_ring) or
+                pygame.sprite.collide_rect(self, outer_ring)
         ): return True
 
-    def isCollidingDirectional(self, left, right):
-        return left < right
+def RectPoints(i_r, o_r):
+    inner_shape_left = [i_r.left, i_r.center[1]]
+    inner_shape_left_rr = (i_r.bottomleft[1] - i_r.topleft[1]) / math.sqrt(
+        (i_r.bottomleft[0] - i_r.topleft[0]) ** 2 + (
+                i_r.bottomleft[1] - i_r.topleft[1]) ** 2)
+
+    inner_shape_right = [i_r.right, i_r.center[1]]
+    inner_shape_right_rr = (i_r.bottomright[1] - i_r.topright[1]) / math.sqrt(
+        (((i_r.bottomright[0] - i_r.topright[0]) ** 2) + (
+                (i_r.bottomright[1] - i_r.topright[1]) ** 2)))
+
+    inner_shape_top = [i_r.center[0], i_r.top]
+    inner_shape_top_rr = (i_r.topleft[1] - i_r.topright[1]) / math.sqrt(
+        ((i_r.topleft[0] - i_r.topright[0]) ** 2 + (
+                i_r.topleft[1] - i_r.topright[1]) ** 2))
+
+    inner_shape_bottom = [i_r.center[0], i_r.bottom]
+    inner_shape_bottom_rr = (i_r.bottomright[1] - i_r.bottomleft[1]) / math.sqrt(
+        ((i_r.bottomright[0] - i_r.bottomleft[0]) ** 2 + (
+                i_r.bottomright[1] - i_r.bottomleft[1]) ** 2))
+
+    outer_shape_left = [o_r.left, o_r.center[1]]
+    outer_shape_left_rr = (o_r.bottomleft[1] - o_r.topleft[1]) / math.sqrt(
+        (o_r.bottomleft[0] - o_r.topleft[0]) ** 2 + (
+                o_r.bottomleft[1] - o_r.topleft[1]) ** 2)
+
+    outer_shape_right = [o_r.right, o_r.center[1]]
+    outer_shape_right_rr = (o_r.bottomright[1] - o_r.topright[1]) / math.sqrt(
+        (((o_r.bottomright[0] - o_r.topright[0]) ** 2) + (
+                (o_r.bottomright[1] - o_r.topright[1]) ** 2)))
+
+    outer_shape_top = [o_r.center[0], o_r.top]
+    outer_shape_top_rr = (o_r.topleft[1] - o_r.topright[1]) / math.sqrt(
+        ((o_r.topleft[0] - o_r.topright[0]) ** 2 + (
+                o_r.topleft[1] - o_r.topright[1]) ** 2))
+
+    outer_shape_bottom = [o_r.center[0], o_r.bottom]
+    outer_shape_bottom_rr = (o_r.bottomright[1] - o_r.bottomleft[1]) / math.sqrt(
+        ((o_r.bottomright[0] - o_r.bottomleft[0]) ** 2 + (
+                o_r.bottomright[1] - o_r.bottomleft[1]) ** 2))
+
+    return [{"left": inner_shape_left, "left_rr": inner_shape_left_rr, "right":inner_shape_right, "right_rr": inner_shape_right_rr, "top": inner_shape_top, "top_rr": inner_shape_top_rr, "bottom": inner_shape_bottom, "bottom_rr": inner_shape_bottom_rr}, \
+{"left": outer_shape_left, "left_rr": outer_shape_left_rr, "right":outer_shape_right, "right_rr": outer_shape_right_rr, "top": outer_shape_top, "top_rr": outer_shape_top_rr, "bottom": outer_shape_bottom, "bottom_rr": outer_shape_bottom_rr}]
+
+def RectPointsDebug(rings):
 
 
-def drawRectDebug(shape):
-    shape.rect = pygame.draw.rect(window, (255, 255, 255), (200, 200, 1520, 680), 5, 100)
+    pygame.draw.circle(window, (255, 0, 0), rings[0]["rect"].center, 10)  # centre
 
-    shape_left = [shape.rect.left, shape.rect.center[1]]
-    shape_left_rr = (shape.rect.bottomleft[1] - shape.rect.topleft[1]) / math.sqrt((shape.rect.bottomleft[0] - shape.rect.topleft[0])**2 + (shape.rect.bottomleft[1] - shape.rect.topleft[1])**2)
+    pygame.draw.circle(window, (255, 0, 0), rings[0]["points"]["left"], 10)  # left
+    pygame.draw.line(window, (255, 0, 0), rings[0]["points"]["left"], (
+    rings[0]["points"]["left"][0] - (50 * math.asin(rings[0]["points"]["left_rr"])),
+    rings[0]["points"]["left"][1] + (50 * math.acos(rings[0]["points"]["left_rr"]))), 5)
 
-    shape_right = [shape.rect.right, shape.rect.center[1]]
-    shape_right_rr = (shape.rect.bottomright[1] - shape.rect.topright[1]) / math.sqrt(
-        (((shape.rect.bottomright[0] - shape.rect.topright[0]) ** 2) + ((shape.rect.bottomright[1] - shape.rect.topright[1]) ** 2)))
-
-
-    shape_top = [shape.rect.center[0], shape.rect.top]
-    shape_top_rr = (shape.rect.topleft[1] - shape.rect.topright[1]) / math.sqrt(
-        ((shape.rect.topleft[0] - shape.rect.topright[0]) ** 2 + (shape.rect.topleft[1] - shape.rect.topright[1]) ** 2))
-
-    shape_bottom = [shape.rect.center[0], shape.rect.bottom]
-    shape_bottom_rr = (shape.rect.bottomright[1] - shape.rect.bottomleft[1]) / math.sqrt(
-        ((shape.rect.bottomright[0] - shape.rect.bottomleft[0]) ** 2 + (shape.rect.bottomright[1] - shape.rect.bottomleft[1]) ** 2))
-
-
-
-    pygame.draw.circle(window, (255, 0, 0), shape.rect.center, 10) # centre
-
-    pygame.draw.circle(window, (255, 0, 0), shape_left, 10) # left
-    pygame.draw.line(window, (255, 0, 0), shape_left, (shape_left[0]-(50*math.asin(shape_left_rr)), shape_left[1]+(50*math.acos(shape_left_rr))), 5)
-
-
-    pygame.draw.circle(window, (255, 0, 0), shape_right, 10) # right
-    pygame.draw.line(window, (255, 0, 0), shape_right,
-                     (shape_right[0] + (50 * math.asin(shape_right_rr)), shape_right[1] + (50 * math.acos(shape_right_rr))),
+    pygame.draw.circle(window, (255, 0, 0), rings[0]["points"]["right"], 10)  # right
+    pygame.draw.line(window, (255, 0, 0), rings[0]["points"]["right"],
+                     (rings[0]["points"]["right"][0] + (50 * math.asin(rings[0]["points"]["right_rr"])),
+                      rings[0]["points"]["right"][1] + (50 * math.acos(rings[0]["points"]["right_rr"]))),
                      5)
 
-    pygame.draw.circle(window, (255, 0, 0), shape_top, 10) # top
-    pygame.draw.line(window, (255, 0, 0), shape_top,
-                     (shape_top[0] + (50 * math.asin(shape_top_rr)), shape_top[1] - (50 * math.acos(shape_top_rr))),
+    pygame.draw.circle(window, (255, 0, 0), rings[0]["points"]["top"], 10)  # top
+    pygame.draw.line(window, (255, 0, 0), rings[0]["points"]["top"],
+                     (rings[0]["points"]["top"][0] + (50 * math.asin(rings[0]["points"]["top_rr"])),
+                      rings[0]["points"]["top"][1] - (50 * math.acos(rings[0]["points"]["top_rr"]))),
                      5)
 
-    pygame.draw.circle(window, (255, 0, 0), shape_bottom, 10) # bottom
-    pygame.draw.line(window, (255, 0, 0), shape_bottom,
-                     (shape_bottom[0] + (50 * math.asin(shape_bottom_rr)), shape_bottom[1] + (50 * math.acos(shape_bottom_rr))),
+    pygame.draw.circle(window, (255, 0, 0), rings[0]["points"]["bottom"], 10)  # bottom
+    pygame.draw.line(window, (255, 0, 0), rings[0]["points"]["bottom"],
+                     (rings[0]["points"]["bottom"][0] + (50 * math.asin(rings[0]["points"]["bottom_rr"])),
+                      rings[0]["points"]["bottom"][1] + (50 * math.acos(rings[0]["points"]["bottom_rr"]))),
                      5)
 
+    # OUTER RING
+    pygame.draw.circle(window, (255, 0, 0), rings[1]["rect"].center, 10)  # centre
 
+    pygame.draw.circle(window, (255, 0, 0), rings[1]["points"]["left"], 10)  # left
+    pygame.draw.line(window, (255, 0, 0),  rings[1]["points"]["left"], (
+        rings[1]["points"]["left"][0] + (50 * math.asin( rings[1]["points"]["left_rr"])),
+        rings[1]["points"]["left"][1] + (50 * math.acos( rings[1]["points"]["left_rr"]))), 5)
+
+    pygame.draw.circle(window, (255, 0, 0), rings[1]["points"]["right"], 10)  # right
+    pygame.draw.line(window, (255, 0, 0), rings[1]["points"]["right"],
+                     (rings[1]["points"]["right"][0] - (50 * math.asin(rings[1]["points"]["right_rr"])),
+                      rings[1]["points"]["right"][1] + (50 * math.acos(rings[1]["points"]["right_rr"]))),
+                     5)
+
+    pygame.draw.circle(window, (255, 0, 0), rings[1]["points"]["top"], 10)  # top
+    pygame.draw.line(window, (255, 0, 0), rings[1]["points"]["top"],
+                     (rings[1]["points"]["top"][0] + (50 * math.asin(rings[1]["points"]["top_rr"])),
+                      rings[1]["points"]["top"][1] + (50 * math.acos(rings[1]["points"]["top_rr"]))),
+                     5)
+
+    pygame.draw.circle(window, (255, 0, 0), rings[1]["points"]["bottom"], 10)  # bottom
+    pygame.draw.line(window, (255, 0, 0), rings[1]["points"]["bottom"],
+                     (rings[1]["points"]["bottom"][0] + (50 * math.asin(rings[1]["points"]["bottom_rr"])),
+                      rings[1]["points"]["bottom"][1] - (50 * math.acos(rings[1]["points"]["bottom_rr"]))),
+                     5)
 
 
 if __name__ == "__main__":
@@ -157,6 +201,14 @@ if __name__ == "__main__":
     carGroup.add(car)
     radius = 50
 
+    inner_ring, outer_ring = pygame.draw.rect(window, (255, 255, 255), (200, 200, 1520, 680), 5, 100), \
+            pygame.draw.rect(window, (255, 255, 255), (0, 0, 1920, 1080), 5, 100)
+    rings = [
+        {"rect": inner_ring, "points": RectPoints(inner_ring, outer_ring)[0]},
+        {"rect": outer_ring, "points": RectPoints(inner_ring, outer_ring)[1]}
+    ]
+
+
     while True:
         if pygame.key.get_pressed()[pygame.K_ESCAPE]:
             break
@@ -164,22 +216,22 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 break
 
-        # our ring
-        drawRectDebug(inner_ring)
-        #outer_ring.rect = pygame.draw.rect(window, (255, 255, 255), (0, 0, 1920, 1080), 5, 100)
-
-        # carGroup.update()
-        # carGroup.draw(window)
-        #
-        # # #direction
-        # arrow = pygame.draw.line(window, (255, 0, 0), car.rect.center, (
-        #         car.rect.center[0]-(radius * math.cos(car.direction)),
-        #     car.rect.center[1] - (radius * math.sin(car.direction))
-        # ), 5)
+        # our rings
+        rings[0]["rect"], rings[1]["rect"] = pygame.draw.rect(window, (255, 255, 255), (200, 200, 1520, 680), 5, 100), \
+            pygame.draw.rect(window, (255, 255, 255), (0, 0, 1920, 1080), 5, 100)
 
 
+        RectPointsDebug(rings)
+
+        carGroup.update()
+        carGroup.draw(window)
+
+        # #direction
+        arrow = pygame.draw.line(window, (255, 0, 0), car.rect.center, (
+                car.rect.center[0]-(radius * math.cos(car.direction)),
+            car.rect.center[1] - (radius * math.sin(car.direction))
+        ), 5)
 
         pygame.display.flip()
-        window.fill((0,0,0))
+        window.fill((0, 0, 0))
         clock.tick(60)
-
